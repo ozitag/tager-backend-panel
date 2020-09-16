9c7ed5382814758485f13d0bbea0d2bb52f88f64
Vital Ozierski <v.ozierski@ozitag.com>
2020-09-16 13:21:51 +0300

    update / to /info

--- routes/routes.php
+++ routes/routes.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 use OZiTAG\Tager\Backend\Panel\Controllers\PublicController;
 
 Route::group(['prefix' => 'tager/panel'], function () {
-    Route::get('/', [PublicController::class, 'info']);
+    Route::get('/info', [PublicController::class, 'info']);
     Route::get('/page', [PublicController::class, 'page']);
 });
