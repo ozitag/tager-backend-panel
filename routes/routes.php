7bc2a168dcf951d59b8423763f1224fa0b1cb80f
Vital Ozierski <v.ozierski@ozitag.com>
2022-02-14 22:39:45 +0300

    1.8 tager support

--- routes/routes.php
+++ routes/routes.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use OZiTAG\Tager\Backend\Panel\Controllers\PublicController;
 
-Route::group(['prefix' => 'tager/panel'], function () {
+Route::group(['prefix' => 'tager/panel', 'middleware' => ['passport:administrators', 'auth:api']], function () {
     Route::get('/info', [PublicController::class, 'info']);
     Route::get('/page', [PublicController::class, 'page']);
 });
